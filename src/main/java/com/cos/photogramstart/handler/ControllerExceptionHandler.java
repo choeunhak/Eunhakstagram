d6b01a634eaa2c6e0ffdcbc9442dbd3af8048c29
choeunhak <eunhakcho@gmail.com>
2021-06-24 06:04:18 +0900

    깃 테스트

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -11,6 +11,6 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(RuntimeException.class)//모든 runtime익셉션 낚아챔
 	public String validationException(RuntimeException e) {
 		return e.getMessage();
-		
+		// 깃 테스트
 	}
 }
