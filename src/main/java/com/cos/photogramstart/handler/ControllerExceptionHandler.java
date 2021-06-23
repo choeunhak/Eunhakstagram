c627671d632ac0773947a14fd3e36dc547b47e30
choeunhak <eunhakcho@gmail.com>
2021-06-24 06:03:13 +0900

    controlleradvice 등록

---
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.cos.photogramstart.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice//에러처리 낚아채게 handler 등록
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(RuntimeException.class)//모든 runtime익셉션 낚아챔
+	public String validationException(RuntimeException e) {
+		return e.getMessage();
+		
+	}
+}
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.service.AuthService;
@@ -48,7 +47,7 @@ public class AuthController {
 	
 	//회원가입버튼을 누르면 -> auth/signup
 	@PostMapping("/auth/signup")
-	public @ResponseBody String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {//responsebody는 파일이 아닌 데이터를응답해준다
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {//@responsebody는 파일이 아닌 데이터를응답해준다
 		//이렇게만 하면 403에러가 뜸
 		//security가 csrf로 1차적으로막는거다
 		//토큰이 있나검사를한다
@@ -57,17 +56,17 @@ public class AuthController {
 			
 			for(FieldError error:bindingResult.getFieldErrors()) {
 				errorMap.put(error.getField(), error.getDefaultMessage());
-				System.out.println(error.getDefaultMessage());
+				//System.out.println(error.getDefaultMessage());
 			}
-			
-			return "오류남";
+			//return "오류남";
+			throw new RuntimeException("유효성검사실패");
 		}else {
 			//log.info(signupDto.toString());
 			User user = signupDto.toEntity();
 			
 			User userEntity = authService.회원가입(user);
 			//System.out.println(userEntity);
-			log.info(user.toString());
+			//log.info(user.toString());
 			return "auth/signin";
 		}
 		
