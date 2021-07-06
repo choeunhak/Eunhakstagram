cf78d8ee1855aff829c534409c93d954f651cb74
choeunhak <02069@naver.com>
2021-07-06 22:39:12 +0900

    chore: spring security에서 세션정보확인

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,7 +1,9 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,8 +19,18 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 
+		
+		
+		//spring security에서 세션정보확인
+		//System.out.println("세션정보:"+ principalDetails.getUser());
+		
+		
+		
+		//직접찾은세션정보, 극혐
+		//Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//System.out.println(auth.getPrincipal());
 		return "user/update";
 	}
 
