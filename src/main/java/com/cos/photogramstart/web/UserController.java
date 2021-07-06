a7e57042aa522c50d36894e9c9990cfe40be14b8
choeunhak <02069@naver.com>
2021-07-06 22:46:57 +0900

    feat: getattribute활용하여 회원정보 수정 기능

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,9 +1,8 @@
 package com.cos.photogramstart.web;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -19,7 +18,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
 
 		
 		
@@ -31,6 +30,8 @@ public class UserController {
 		//직접찾은세션정보, 극혐
 		//Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		//System.out.println(auth.getPrincipal());
+		
+		model.addAttribute("principal", principalDetails.getUser());
 		return "user/update";
 	}
 
