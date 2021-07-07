d2ebf0ce5c24eab923df52737441a313fe757c33
choeunhak <02069@naver.com>
2021-07-07 14:29:49 +0900

    feat:JPA resultmapper를 위한 qlrm 라이브러리 등록

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -2,7 +2,10 @@ package com.cos.photogramstart.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 public class SubscribeService {
 	
 	private final SubscribeRepository subscribeRepository;
+	private final EntityManager em;//복잡한 네이티브 쿼리를 위해 가져옴, 모든 repository이 em을 구현해서 만들어져잇긴때문에 직접 구현
+	
 	
 	@Transactional
 	public void 구독하기(int fromUserId, int toUserId) {
@@ -34,10 +39,24 @@ public class SubscribeService {
 	}
 
 	@Transactional(readOnly=true)
-	public List<SubscribeDto> 구독리스트(int principalid, int pageUserId) {
+	public List<SubscribeDto> 구독리스트(int principalId, int pageUserId) {
 		
 		
-		return null;
+		StringBuffer sb = new StringBuffer();
+		sb.append("select u.id userId, u.username, u.profileImageUrl,  ");
+		sb.append("if((select 1 from subscribe where fromUserId = ? and toUserId = u.id), 1, 0) subscribeState, ");  // 로그인한 아이디principalDetails.user.id
+		sb.append("if(?=u.id, 1, 0) equalUserState "); // 로그인한 아이디, principalDetails.user.id
+		sb.append("from subscribe f inner join user u on u.id = f.toUserId ");
+		sb.append("where f.fromUserId = ? "); // 마지막물음표pageUserId
+		
+		Query query = em.createNativeQuery(sb.toString())
+				.setParameter(1, principalId)
+				.setParameter(2, principalId)
+				.setParameter(3, pageUserId);
+		
+		JpaResultMapper result  = new JpaResultMapper();
+		List<SubscribeDto> subscribeDtos = result.list(query, SubscribeDto.class);
+		return subscribeDtos;
 	}
 
 }
