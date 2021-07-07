cc70d988d15ffd8c8ff502e9281dbd3eb233cc6d
choeunhak <02069@naver.com>
2021-07-07 13:09:44 +0900

    feat: 프로필 페이지 완성을 위해 userprofiledto 생성

--- src/main/java/com/cos/photogramstart/service/UserService.java
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -9,6 +9,7 @@ import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
+import com.cos.photogramstart.web.dto.user.UserProfileDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,13 +23,17 @@ public class UserService {
 	
 	
 	@Transactional(readOnly=true)//리드오니가 jpa를 일을 덜하게 해줄수있다(계속 감지하지않게함)
-	public User 회원프로필(int userId) {
+	public UserProfileDto 회원프로필(int pageUserId, int principalId) {
+		UserProfileDto dto = new UserProfileDto();
 		
-		User userEntity = userRepository.findById(userId).orElseThrow(()->{
+		User userEntity = userRepository.findById(pageUserId).orElseThrow(()->{
 			throw new CustomException("해당 프로필 페이지는 없는 페이지입니다.");
 		});
 		
-		return userEntity;
+		dto.setUser(userEntity);
+		dto.setPageOwnerState(pageUserId==principalId);
+		dto.setImageCount(userEntity.getImages().size());
+		return dto;
 	}
 	
 	@Transactional
---
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserProfileDto.java
@@ -0,0 +1,20 @@
+package com.cos.photogramstart.web.dto.user;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileDto {
+	
+	private Boolean pageOwnerState;
+	private int imageCount;
+	private User user;
+
+}
